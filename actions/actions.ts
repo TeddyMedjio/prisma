cf81d30f19928a432d3dd98ba2d50635f2120dc5
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-08-13 08:54:54 +0100

    last update

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,23 +1,34 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { Prisma } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 export async function createPost(formData: FormData) {
-  await prisma.post.create({
-    data: {
-      title: formData.get("title") as string,
-      slug: (formData.get("title") as string)
-        .replace(/\s+/g, "-")
-        .toLowerCase(),
-      content: formData.get("content") as string,
-      author: {
-        connect: {
-          email: "john@gmail.com",
+  try {
+    await prisma.post.create({
+      data: {
+        title: formData.get("title") as string,
+        slug: (formData.get("title") as string)
+          .replace(/\s+/g, "-")
+          .toLowerCase(),
+        content: formData.get("content") as string,
+        author: {
+          connect: {
+            email: "john@gmail.com",
+          },
         },
       },
-    },
-  });
+    });
+  } catch (error: any) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        console.log(
+          "There is a unique contraint violation, a new user cannot be created with this email"
+        );
+      }
+    }
+  }
   revalidatePath("/posts");
 }
 
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,36 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-async function main() {}
+
+const initialPosts: Prisma.PostCreateInput[] = [
+  {
+    title: "Post 1",
+    slug: "post-1",
+    content: "This is the content of post 1",
+    author: {
+      connectOrCreate: {
+        where: {
+          email: "john@gmail.com",
+        },
+        create: {
+          email: "john@gmail.com",
+          hashedPassword: "wegdssgsdgsdgsdgsdg",
+        },
+      },
+    },
+  },
+];
+async function main() {
+  console.log(`Start seeding ...`);
+
+  for (const post of initialPosts) {
+    const newPost = await prisma.post.create({
+      data: post,
+    });
+    console.log(`Created post with id: ${newPost.id}`);
+  }
+
+  console.log(`Seeding finished`);
+}
 main()
   .then(async () => {
     await prisma.$disconnect();
